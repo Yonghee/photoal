6a49ee93dc2fc38ea1d9106aa2efdfa5c826b2dd
nomad <goodyhlee@coupang.com>
2017-12-26 15:55:57 +0900

    create project via spring.io

---
+++ src/test/java/com/goodyhlee/photoal/photoal/PhotoalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.goodyhlee.photoal.photoal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
